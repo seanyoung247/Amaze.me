61a203791aadc47fdf9fd15011ffef14c1e155e6
Sean Young <seanyoung247@gmail.com>
2020-10-09 22:07:54 -0400

    Canvas resolution setting code
    Added test code for resizing the canvas element's resolution when the window resizes.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,10 @@
 $( document ).ready(function() {
   var gameCanvas = document.getElementById("gameCanvas");
-  //var ctx = gameCanvas.getContext("2d");
+  var ctx = gameCanvas.getContext("2d");
+
+  //Idea for responsive canvas resolution:
+  ctx.canvas.width = $("#gameDiv").innerWidth() - 30;
+  ctx.canvas.height = $("#gameDiv").innerHeight() - 30;
 
   var cam = new Camera(1.5,1.5,1,0);
   var map = new RayMap2(normalMap);
